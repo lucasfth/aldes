83b967cdf2bca443e1f5803ba4dc01676bffbce4
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2024-10-23 14:10:08 +0200

    Add whole Graph sol

--- red_scare/sol/bfs.py
+++ red_scare/sol/bfs.py
@@ -1,32 +1,85 @@
-def BFS(self, s, t, parent):
+class Graph:
+ 
+    def __init__(self, adj_list, capacity):
+        self.adj_list = adj_list
+        self.capacity = capacity
+        # self.COL = len(gr[0])
+ 
+    '''Returns true if there is a path from source 's' to sink 't' in
+    residual graph. Also fills parent[] to store the path '''
+ 
+    def BFS(self, s, t, parent):
+ 
+        # Mark all the vertices as not visited
+        visited = {s}
+ 
+        # Create a queue for BFS
+        queue = deque([s])
+ 
+         # Standard BFS Loop
+        while queue:
+ 
+            # Dequeue a vertex from queue and print it
+            u = queue.popleft()
+ 
+            # Get all adjacent vertices of the dequeued vertex u
+            # If a adjacent has not been visited, then mark it
+            # visited and enqueue it
+            for v in self.adj_list[u]:
+                if v not in visited and self.capacity[u][v] > 0:
+                      # If we find a connection to the sink node, 
+                    # then there is no point in BFS anymore
+                    # We just have to set its parent and can return true
+                    queue.append(v)
+                    visited.add(v)
+                    parent[v] = u
+                    if v == sink:
+                        return True
+ 
+        # We didn't reach sink in BFS starting 
+        # from source, so return false
+        return False
+             
+     
+    # Returns the maximum flow from s to t in the given graph
+    def FordFulkerson(self, source, sink):
 
-    # Mark all the vertices as not visited
-    visited = {s}
+        solution = {}
+ 
+        # This array is filled by BFS and to store path
+        parent = {}
+ 
+        max_flow = 0 # There is no flow initially
 
-    # Create a queue for BFS
-    queue = deque([s])
+        processed = set()
+ 
+        # Augment the flow while there is path from source to sink
+        while self.BFS(source, sink, parent):
+ 
+            # Find minimum residual capacity of the edges along the
+            # path filled by BFS. Or we can say find the maximum flow
+            # through the path found.
+            path_flow = float("Inf")
+            s = sink
+            while(s !=  source):
+                path_flow = min (path_flow, self.capacity[parent[s]][s])
+                s = parent[s]
+ 
+            # Add path flow to overall flow
+            max_flow +=  path_flow
+ 
+            # update residual capacities of the edges and reverse edges
+            # along the path
+            v = sink
+            while(v !=  source):
+                u = parent[v]
+                self.capacity[u][v] -= path_flow
+                self.capacity[v][u] += path_flow
+                # if u < n and v >= sink + 1 and u not in processed:
+                #     solution[u] = edge_label[(u, v)]
+                #     processed.add(u)
+                v = u
 
-        # Standard BFS Loop
-    while queue:
-
-        # Dequeue a vertex from queue and print it
-        u = queue.popleft()
-
-        # Get all adjacent vertices of the dequeued vertex u
-        # If a adjacent has not been visited, then mark it
-        # visited and enqueue it
-        for v in self.adj_list[u]:
-            if v not in visited and self.capacity[u][v] > 0:
-                    # If we find a connection to the sink node, 
-                # then there is no point in BFS anymore
-                # We just have to set its parent and can return true
-                queue.append(v)
-                visited.add(v)
-                parent[v] = u
-                if v == sink:
-                    return True
-
-    # We didn't reach sink in BFS starting 
-    # from source, so return false
-    return False
-            
+            parent = {}
+ 
+        return max_flow

ce796dae88fd505a555435b1a22e97ead1eb3eb6
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2024-10-23 14:27:42 +0200

    Create discussed sol for few.py

---
+++ red_scare/sol/few.py
@@ -0,0 +1,8 @@
+"""
+Discussed solution:
+Use Djikstra's algorithm to find the shortest path from s to t.
+Though the costs to move to black nodes are zero and to red nodes are one.
+
+Return:
+The minimum number of red nodes you can have in a path from s to t.
+"""

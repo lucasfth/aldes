ff5d4938638da8fc0f438b3bdbc3aeb6c26c7cec
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2024-10-30 10:25:28 +0100

    Add alternate desc

--- red_scare/sol/alternate.py
+++ red_scare/sol/alternate.py
@@ -1,3 +1,8 @@
 """
+Discussed solution:
+Use BFS or something similar with an extra constraint in terms of
+how you can explore the graph.
 
+Return:
+Truth value of whether you can find a path from s to t with an alternating path.
 """

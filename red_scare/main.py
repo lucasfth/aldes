e9ecd11bdc03c0ccce82ad0b15a5dcb33b40d057
Lucas Frey Torres Hanson <lucasfth@bjelke-torres.com>
2024-11-20 15:31:09 +0100

    Add timing of exec

--- red_scare/main.py
+++ red_scare/main.py
@@ -3,6 +3,8 @@ File that will:
 Load graphs for all generated inputs in `./data` and run the solution on them one at a time.
 '''
 
+import time
+from tabulate import tabulate
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), './sol'))
@@ -16,12 +18,21 @@ from some import some # type: ignore
 def main():
     files = os.listdir('./data')
     problems = [none(), alternate(), few(), some()]
+    running_time = [["File"] + [p.__class__.__name__ for p in problems]]
 
     for file in files:
         if file.endswith('.txt'):
             print('Loading', file)
+            row = [file]
             for problem in problems:
+                s_t = time.time()
                 problem.run("./data/" + file)
+                e_t = time.time()
+                delta = e_t - s_t
+                row.append(f"{delta:.6f}s")
+            running_time.append(row)
+
+    print(tabulate(running_time, headers="firstrow", tablefmt="grid"))
 
 if __name__=="__main__":
     main()
